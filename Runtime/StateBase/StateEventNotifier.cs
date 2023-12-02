2f2b843ac1a2aae7df65e98648b6a1f18ca71242
david2toranzo2 <38369752+david-toranzo@users.noreply.github.com>
2023-12-01 23:18:50 -0300

    [A] add state event notifier

---
+++ Runtime/StateBase/StateEventNotifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Patterns.StateMachine
+{
+    public class StateEventNotifier : StateBase
+    {
+        public Action OnStateEnter { get; set; }
+        public Action OnStateExit { get; set; }
+        public Action OnStateStay{ get; set; }
+
+        public override void Enter() => OnStateEnter?.Invoke();
+        public override void Exit() => OnStateExit?.Invoke();
+        protected override void ProcessWorkActualState() => OnStateStay?.Invoke();
+    }
+}
